8c8c726f6640b4ba8f11b50334174b8a34d6dad8
yongler <leeyongler1.618@gmail.com>
2023-01-24 11:16:04 +0800

    Project setup

---
+++ Peggle/Peggle/PeggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeggleApp.swift
+//  Peggle
+//
+//  Created by Lee Yong Ler on 24/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PeggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
